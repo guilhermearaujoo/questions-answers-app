9371653c77e00e2502f0482524caf4e4ce19541f
guilherme <ghma.em1@gmail.com>
2023-06-24 19:49:36 -0300

    Cria testes para rota POST /respostas

--- backend/src/test/integration/resposta.test.ts
+++ backend/src/test/integration/resposta.test.ts
@@ -7,6 +7,7 @@ import chaiHttp = require('chai-http');
 import App from '../../App';
 import SequelizeTest from '../../database/models/SequelizeResposta';
 import { resposta, respostas } from '../mocks/Resposta.mock';
+import Validations from '../../middlewares/Validations';
 
 chai.use(chaiHttp);
 
@@ -40,6 +41,53 @@ describe('Respostas Test', function () {
 
     expect(status).to.equal(404);
     expect(body.message).to.equal('Resposta 1 not found');
+  });
+
+  it('should create a resposta', async function() {
+    sinon.stub(SequelizeTest, 'create').resolves(resposta as any);
+    sinon.stub(Validations, 'validateResposta').returns();
+
+    const { id, ...sendData } = resposta;
+
+  const { status, body } = await chai.request(app).post('/respostas')
+      .send(sendData);
+
+    expect(status).to.equal(201);
+    expect(body).to.deep.equal(resposta);
+  });
+
+
+  describe('shouldn\'t create a resposta with invalid body data', async function() {
+    it('Shouldn\'t create a resposta with invalid enqueteId', async function() {
+      const { status, body } = await chai.request(app).post('/respostas')
+        .send({
+          resposta: "resposta",
+          "enqueteId": "1",
+        });
+  
+      expect(status).to.equal(422);
+      expect(body.message).to.equal('enqueteId must be a number');
+    });
+
+    it('Shouldn\'t create a resposta without resposta', async function() {
+      const { status, body } = await chai.request(app).post('/respostas')
+        .send({
+          "enqueteId": 1
+        });
+  
+      expect(status).to.equal(400);
+      expect(body.message).to.equal('resposta is required');
+    });
+
+    it('Shouldn\'t create a resposta without enqueteId', async function() {
+      const { status, body } = await chai.request(app).post('/respostas')
+        .send({
+          "resposta": "resposta"
+        });
+  
+      expect(status).to.equal(400);
+      expect(body.message).to.equal('enqueteId is required');
+    });
   });
 
   afterEach(sinon.restore);
